77d0fe349f653e04ddb9c4f13a116b515f2f4bfa
Mohamed Aziz Zouaghia <mohamedaziz.zouaghia@neoxam.com>
2025-05-18 11:52:21 +0100

    renames

--- back/src/main/java/com/GitProjectApplication.java
+++ back/src/main/java/com/GitProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class aziz_zouaghia_tpfoyerApplication {
+public class GitProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(aziz_zouaghia_tpfoyerApplication.class, args);
+		SpringApplication.run(GitProjectApplication.class, args);
 	}
 
 }
